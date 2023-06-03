653f3575e73ac0a8a409f660e1d35a6823c8abbb
Ulyana-AA <andreeva.ulyanakpc572020@gmail.com>
2023-06-03 18:30:55 +0300

    заголовок = textbox

---
+++ upi/ViewModels/MainWIndowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace upi.ViewModels
+{
+    internal class MainWIndowViewModel: ViewModel
+    {
+        public string _Title = "Ой, я кажется лопухнулся";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
