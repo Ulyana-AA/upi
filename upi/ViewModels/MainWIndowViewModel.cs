6035a29049b7548ddde666f4756ee0a958dd0551
Ulyana-AA <98107469+Ulyana-AA@users.noreply.github.com>
2023-06-05 23:12:32 +0300

    Update MainWIndowViewModel.cs

--- upi/ViewModels/MainWIndowViewModel.cs
+++ upi/ViewModels/MainWIndowViewModel.cs
@@ -6,12 +6,6 @@ namespace upi.ViewModels
 {
     internal class MainWIndowViewModel: ViewModel
     {
-        public string _Title = "Ой, я кажется лопухнулся";
-
-        public string Title
-        {
-            get => _Title;
-            set => Set(ref _Title, value);
-        }
+        
     }
 }
